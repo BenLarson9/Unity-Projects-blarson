b1632c01d7089c26d1d93f0e394cb02115080693
BenLarson9 <54853304+BenLarson9@users.noreply.github.com>
2020-09-09 10:50:13 -0500

    lesson 1.1, 1.2 complete, part of 1.3

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   
+
+
+    public float speed = 5f;
+  
+
+
+    void Update()
+    {
+        //this is dumb 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
