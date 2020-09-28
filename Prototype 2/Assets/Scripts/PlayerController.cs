ba22f40e8a96d1e61171a2e3df55f392cc5d3d59
BenLarson9 <54853304+BenLarson9@users.noreply.github.com>
2020-09-28 14:41:56 -0500

    lesson 8 its called completed, finally pushed

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,10 @@ public class PlayerController : MonoBehaviour
 
     public float horizontalInput;
     public float speed = 10f;
+    /*he didn't tell me how to set up positions, so i set it on the z, and for some reason, the 
+    z coordinate is not centered, so i have to have 2 separate variables and im not redoing it*/
+    public float zRangeup = 20;
+    public float zRangdown = 0;
 
 
 
@@ -15,5 +19,15 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+
+        if (transform.position.z > 20)
+           {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeup);
+           }
+        if(transform.position.z < 0)
+           {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangdown);
+           }
     }
 }
