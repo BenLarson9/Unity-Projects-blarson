8b5632f28a3066e22cd3aea5be4aaece9e2af81b
BenLarson9 <54853304+BenLarson9@users.noreply.github.com>
2020-10-07 10:08:00 -0500

    lab 3

---
+++ Ben Larson - Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed;
+    private Rigidbody playerRb;
+    private float verticalInput;
+    private float horizontalInput;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>():
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GetKey(KeyCode.UpArrow))
+    }
+}
