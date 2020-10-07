35706cba0567d4395eb8c123719cb6d79f521b8d
BenLarson9 <54853304+BenLarson9@users.noreply.github.com>
2020-10-07 10:24:52 -0500

    yepp thats lab 3

--- Ben Larson - Personal Project/Assets/Scripts/PlayerController.cs
+++ Ben Larson - Personal Project/Assets/Scripts/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        playerRb = GetComponent<Rigidbody>():
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
